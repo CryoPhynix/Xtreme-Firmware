ac914109f7021e893d0fb20e95598426df1d3c4c
kirkpe <kirkperkins99@gmail.com>
2023-11-21 16:37:28 -0800

    Added new config scene update

--- applications/main/bad_kb/scenes/bad_kb_scene_config_bt_mac.c
+++ applications/main/bad_kb/scenes/bad_kb_scene_config_bt_mac.c
@@ -1,5 +1,19 @@
 #include "../bad_kb_app.h"
 
+typedef struct {
+    char mac_address[BAD_KB_MAC_LEN];
+    char nickname[32];
+} MacNickname;
+
+#define MAX_MAC_NICKNAMES 10
+
+typedef struct {
+    MacNickname nicknames[MAX_MAC_NICKNAMES];
+    int num_nicknames;
+} MacNicknameMenu;
+
+MacNicknameMenu mac_nickname_menu;
+
 void bad_kb_scene_config_bt_mac_byte_input_callback(void* context) {
     BadKbApp* bad_kb = context;
 
@@ -47,4 +61,44 @@ void bad_kb_scene_config_bt_mac_on_exit(void* context) {
 
     byte_input_set_result_callback(byte_input, NULL, NULL, NULL, NULL, 0);
     byte_input_set_header_text(byte_input, "");
+}
+
+void bad_kb_scene_config_bt_mac_submenu_create_nickname(void* context) {
+    BadKbApp* bad_kb = context;
+    MacNicknameMenu* menu = &mac_nickname_menu;
+
+    if (menu->num_nicknames >= MAX_MAC_NICKNAMES) {
+        // Maximum number of nicknames reached
+        return;
+    }
+
+    // Get the current MAC address
+    char mac_address[BAD_KB_MAC_LEN];
+    memmove(mac_address, bad_kb->config.bt_mac, BAD_KB_MAC_LEN);
+    furi_hal_bt_reverse_mac_addr(mac_address);
+
+    // Prompt the user for a nickname
+    char nickname[32];
+    // Code to prompt the user for a nickname and store it in the 'nickname' variable
+
+    // Add the MAC address and nickname to the menu
+    memmove(menu->nicknames[menu->num_nicknames].mac_address, mac_address, BAD_KB_MAC_LEN);
+    memmove(menu->nicknames[menu->num_nicknames].nickname, nickname, strlen(nickname));
+    menu->num_nicknames++;
+}
+
+void bad_kb_scene_config_bt_mac_submenu_switch_nickname(void* context) {
+    BadKbApp* bad_kb = context;
+    MacNicknameMenu* menu = &mac_nickname_menu;
+
+    // Code to display the list of nicknames and prompt the user to select one
+
+    // Get the selected nickname
+    int selected_index = 0; // Index of the selected nickname in the menu
+    char selected_mac_address[BAD_KB_MAC_LEN];
+    memmove(selected_mac_address, menu->nicknames[selected_index].mac_address, BAD_KB_MAC_LEN);
+
+    // Update the BT MAC address in the configuration
+    memmove(bad_kb->config.bt_mac, selected_mac_address, BAD_KB_MAC_LEN);
+    bad_kb_config_refresh(bad_kb);
 }
